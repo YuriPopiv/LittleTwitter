2c8b545ebec5b6fa1b39e437ab8d996cd45c5cdf
YuriPopiv <popiv.yuri@gmail.com>
2017-05-29 18:44:34 +0300

    up

--- app/src/main/java/com/example/yura9/littletwitter/MainActivity.java
+++ app/src/main/java/com/example/yura9/littletwitter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.yura9.littletwitter;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,10 +20,13 @@ import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
 import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.core.services.StatusesService;
 import com.twitter.sdk.android.tweetui.CompactTweetView;
+import com.twitter.sdk.android.tweetui.Timeline;
+import com.twitter.sdk.android.tweetui.TimelineResult;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.TweetUtils;
 import com.twitter.sdk.android.tweetui.TweetView;
@@ -44,6 +48,15 @@ public class MainActivity extends AppCompatActivity {
     private static final String TWITTER_KEY = "	wNCNyDrvtjAiSkygZhFUKlLKL";
     private static final String TWITTER_SECRET = "d28iZAHo9XfbcDyLseesRrB7QsDmqxGd0DiDXB82ryBXSogTkR";
     private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+   // private final Context context;
+    static long CAPACITY = 200L;
+    // timeline that next and previous items are loaded from
+    //private Timeline<Tweet> timeline;
+    private  UserTimeline timeline;
+    private TimelineHolder timelineHolder;
+    private TweetTimelineAdapter adapter;
+    private List<Tweet> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mRecyclerView = (RecyclerView)findViewById(R.id.recycler);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
@@ -62,18 +76,123 @@ public class MainActivity extends AppCompatActivity {
         /**
          * @param userTimeLine which shows the @QWERTY user’s timeline of Tweets
          */
-        final UserTimeline userTimeline = new UserTimeline.Builder()
+        timeline = new UserTimeline.Builder()
                 .screenName("QWERTY")
                 .build();
 
-        final TweetTimelineAdapter adapter = new TweetTimelineAdapter(this, userTimeline);
+        timelineHolder = new TimelineHolder();
+        itemList = new ArrayList<>();
+        refresh();
+
+        adapter = new TweetTimelineAdapter(MainActivity.this, timeline, itemList){
+            @Override
+            public Tweet getItem(int position) {
+                if (isLastPosition(position)) {
+                    previous();
+                }
+                return itemList.get(position);}
+            };
 
         mRecyclerView.setAdapter(adapter);
 
 
     }
 
-    
+    boolean withinMaxCapacity(){return itemList.size() < CAPACITY;}
+
+    /**
+     * Triggers loading the latest items. If items are
+     * received, they replace existing items.
+     */
+    public void refresh(){
+        //reset timelineHolder cursors to be null, loadNext will get latest items
+        timelineHolder.resetCursors();
+        loadNext(timelineHolder.positionForNext(), new RefreshCallback());
+    }
+
+    /**
+     * Triggers loading previous items.
+     */
+    public void previous(){
+        loadPrevious(timelineHolder.positionForPrevious(), new PreviousCallback());
+    }
+
+    /**
+     * Checks the capacity and sets requestInFlight before calling timeline.next.
+     */
+    void loadNext(Long minPosition, Callback<TimelineResult<Tweet>> callback){
+        if (withinMaxCapacity()){
+            if (timelineHolder.startTimelineRequest()){
+                timeline.next(minPosition, callback);
+            }else {
+                callback.failure(new TwitterException("Request already in flight"));
+            }
+        }else {
+            callback.failure(new TwitterException("Max capacity reached"));
+        }
+    }
+
+    /**
+     * Checks the capacity and sets requestInFlight before calling timeline.previous.
+     */
+    void loadPrevious(Long maxPosition, Callback<TimelineResult<Tweet>> callback){
+        if (withinMaxCapacity()){
+            if (timelineHolder.startTimelineRequest()){
+                timeline.previous(maxPosition, callback);
+            }else {
+                callback.failure(new TwitterException("Request already in flight"));
+            }
+        }else {
+            callback.failure(new TwitterException("Max capacity reached"));
+        }
+    }
+    boolean isLastPosition(int position){ return position == (itemList.size() - 1);}
+
+    /**
+     * Handles receiving latest timeline items, clears listItems, and sets received items.
+     */
+    class RefreshCallback extends Callback<TimelineResult<Tweet>> {
+
+        @Override
+        public void success(Result<TimelineResult<Tweet>> result) {
+            if (result.data.items.size() > 0) {
+                timelineHolder.finishTimeLineRequest();
+                itemList.clear();
+                itemList.addAll(result.data.items);
+                adapter.notifyDataSetChanged();
+                timelineHolder.setNextCursor(result.data.timelineCursor);
+
+            }
+
+        }
+
+        @Override
+        public void failure(TwitterException exception) {
+            timelineHolder.finishTimeLineRequest();
+        }
+    }
+
+    /**
+     * Handles appending listItems and updating the scrollStateHolder previousCursor.
+     */
+    class PreviousCallback extends Callback<TimelineResult<Tweet>> {
+
+        @Override
+        public void success(Result<TimelineResult<Tweet>> result) {
+            if (result.data.items.size() > 0) {
+                timelineHolder.finishTimeLineRequest();
+                itemList.addAll(result.data.items);
+                adapter.notifyDataSetChanged();
+                timelineHolder.setPreviousCursor(result.data.timelineCursor);
+            }
+        }
+
+        @Override
+        public void failure(TwitterException exception) {
+            timelineHolder.finishTimeLineRequest();
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/example/yura9/littletwitter/TweetTimelineAdapter.java
+++ app/src/main/java/com/example/yura9/littletwitter/TweetTimelineAdapter.java
@@ -3,11 +3,17 @@ package com.example.yura9.littletwitter;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.internal.UserUtils;
 import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.tweetui.CompactTweetView;
 import com.twitter.sdk.android.tweetui.Timeline;
@@ -20,215 +26,61 @@ import java.util.List;
  * Created by yura9 on 5/23/2017.
  */
 
-class TweetTimelineAdapter extends RecyclerView.Adapter<TweetTimelineAdapter.TweetViewHolder>{
+abstract class TweetTimelineAdapter extends RecyclerView.Adapter<TweetTimelineAdapter.TweetViewHolder>{
     private final Context context;
-    static long CAPACITY = 200L;
-    // timeline that next and previous items are loaded from
-    final Timeline<Tweet> timeline;
-    final TimelineHolder timelineHolder;
     List<Tweet> itemList;
 
 
-    public TweetTimelineAdapter(Context context, Timeline<Tweet> timeline){
+    public TweetTimelineAdapter(Context context, Timeline<Tweet> timeline, List<Tweet> tweets){
         if (timeline == null)
             throw new IllegalArgumentException("Timeline must not be null");
         this.context = context;
-        this.timeline = timeline;
-        timelineHolder = new TimelineHolder();
-        itemList = new ArrayList<>();
-        refresh(null);
-
+        itemList = tweets;
     }
 
 
     @Override
     public TweetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Tweet tweet = getItem(0);
-        CompactTweetView tweetView = new CompactTweetView(context, tweet);
-        return new TweetViewHolder(tweetView);
+        View v = LayoutInflater.from(context).inflate(R.layout.tweet_custom, parent, false);
+        TweetViewHolder holder = new TweetViewHolder(v);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(TweetViewHolder holder, int position) {
         Tweet tweet = getItem(position);
-        CompactTweetView tweetView = (CompactTweetView) holder.itemView;
-        tweetView.setTweet(tweet);
+        holder.author_full_name.setText(tweet.user.name);
+        holder.tweet_text.setText(tweet.text);
+        holder.setAuthorPhoto(tweet);
 
     }
-
 
     @Override
     public int getItemCount() {
         return itemList.size();
     }
 
+    public abstract Tweet getItem(int position);
 
-    @Override
-    public void registerAdapterDataObserver(RecyclerView.AdapterDataObserver observer) {
-        super.registerAdapterDataObserver(observer);
-    }
+    public  class TweetViewHolder extends RecyclerView.ViewHolder{
 
-    static final class TweetViewHolder extends RecyclerView.ViewHolder{
-
-        public TweetViewHolder(CompactTweetView tweetView){
+        public ImageView author_avatar;
+        public TextView author_full_name;
+        public TextView tweet_text;
+        public TweetViewHolder(View tweetView){
             super(tweetView);
-        }
-    }
-
-    boolean withinMaxCapacity(){return itemList.size() < CAPACITY;}
-
-    /**
-     * Triggers loading the latest items. If items are
-     * received, they replace existing items.
-     */
-    public void refresh(Callback<TimelineResult<Tweet>> developerCb){
-
-        //reset timelineHolder cursors to be null, loadNext will get latest items
-        timelineHolder.resetCursors();
-        loadNext(timelineHolder.positionForNext(), new RefreshCallback(developerCb, timelineHolder));
-    }
-
-    /**
-     * Triggers loading previous items.
-     */
-    public void previous(){
-        loadPrevious(timelineHolder.positionForPrevious(), new PreviousCallback(timelineHolder));
-    }
-
-    /**
-     * Checks the capacity and sets requestInFlight before calling timeline.next.
-     */
-    void loadNext(Long minPosition, Callback<TimelineResult<Tweet>> callback){
-        if (withinMaxCapacity()){
-            if (timelineHolder.startTimelineRequest()){
-                timeline.next(minPosition, callback);
-            }else {
-                callback.failure(new TwitterException("Request already in flight"));
-            }
-        }else {
-            callback.failure(new TwitterException("Max capacity reached"));
-        }
-    }
-
-    /**
-     * Checks the capacity and sets requestInFlight before calling timeline.previous.
-     */
-    void loadPrevious(Long maxPosition, Callback<TimelineResult<Tweet>> callback){
-        if (withinMaxCapacity()){
-            if (timelineHolder.startTimelineRequest()){
-                timeline.previous(maxPosition, callback);
-            }else {
-                callback.failure(new TwitterException("Request already in flight"));
-            }
-        }else {
-            callback.failure(new TwitterException("Max capacity reached"));
-        }
-    }
-
-    /**
-     * Gets the data item associated with the specified position in the data set.
-     */
-    public Tweet getItem(int position){
-        if (isLastPosition(position)) {
-            previous();
-        }
-        return itemList.get(position);}
-
-    boolean isLastPosition(int position){ return position == (itemList.size() - 1);}
-
-
-    /**
-     * TimelineDelegate.DefaultCallback is a Callback which handles setting requestInFlight to
-     * false on both success and failure and calling through to a wrapped developer Callback.
-     * Subclass methods must call through to the parent method after their custom implementation.
-     */
-    class DefaultCallback extends Callback<TimelineResult<Tweet>>{
-        final Callback<TimelineResult<Tweet>> developerCallback;
-        final TimelineHolder timelineHolder;
-
-        DefaultCallback(Callback<TimelineResult<Tweet>> developerCb, TimelineHolder timelineHolder){
-            this.developerCallback = developerCb;
-            this.timelineHolder = timelineHolder;
+            author_avatar = (ImageView) tweetView.findViewById(R.id.tweet_author_avatar);
+            author_full_name = (TextView) tweetView.findViewById(R.id.tweet_author_full_name);
+            tweet_text = (TextView) tweetView.findViewById(R.id.tweet_text);
         }
 
-        @Override
-        public void success(Result<TimelineResult<Tweet>> result) {
-            timelineHolder.finishTimeLineRequest();
-            if (developerCallback != null){
-                developerCallback.success(result);
-            }
-        }
-
-        @Override
-        public void failure(TwitterException exception) {
-            timelineHolder.finishTimeLineRequest();
-            if (developerCallback != null){
-                developerCallback.failure(exception);
+        public void setAuthorPhoto(Tweet tweet){
+            final String url = UserUtils.getProfileImageUrlHttps(tweet.user, UserUtils.AvatarSize.REASONABLY_SMALL);
+            if (url != null) {
+                Picasso.with(context).load(url).transform(new PhotoTransformation()).into(author_avatar);
             }
         }
     }
 
-    /**
-     * Handles receiving next timeline items. Prepends received items to listItems, updates the
-     * scrollStateHolder nextCursor, and calls notifyDataSetChanged.
-     */
-    class NextCallback extends DefaultCallback {
 
-        NextCallback(Callback<TimelineResult<Tweet>> developerCb, TimelineHolder timelineHolder){
-            super(developerCb, timelineHolder);
-        }
-
-        @Override
-        public void success(Result<TimelineResult<Tweet>> result) {
-            if (result.data.items.size() > 0){
-                final ArrayList<Tweet> receivedItems = new ArrayList<>(result.data.items);
-                receivedItems.addAll(itemList);
-                itemList = receivedItems;
-                notifyDataSetChanged();
-                timelineHolder.setNextCursor(result.data.timelineCursor);
-            }
-
-            super.success(result);
-        }
-    }
-
-    /**
-     * Handles receiving latest timeline items, clears listItems, and sets received items.
-     */
-    class RefreshCallback extends NextCallback {
-
-        RefreshCallback(Callback<TimelineResult<Tweet>> developerCb,
-                        TimelineHolder timelineStateHolder) {
-            super(developerCb, timelineStateHolder);
-        }
-
-        @Override
-        public void success(Result<TimelineResult<Tweet>> result) {
-            if (result.data.items.size() > 0) {
-                itemList.clear();
-            }
-            super.success(result);
-        }
-    }
-
-    /**
-     * Handles appending listItems and updating the scrollStateHolder previousCursor.
-     */
-    class PreviousCallback extends DefaultCallback {
-
-        PreviousCallback(TimelineHolder timelineStateHolder) {
-            super(null, timelineStateHolder);
-        }
-
-        @Override
-        public void success(Result<TimelineResult<Tweet>> result) {
-            if (result.data.items.size() > 0) {
-                itemList.addAll(result.data.items);
-                notifyDataSetChanged();
-                timelineHolder.setPreviousCursor(result.data.timelineCursor);
-            }
-            // do nothing when zero items are received. Subsequent 'next' call does not change.
-            super.success(result);
-        }
-    }
 }
